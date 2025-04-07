43ea728d42a6a70af4918a3514845fd0cf89bfe3
Pritam Jatkar <pritamjatkar1724@gmail.com>
2025-04-07 20:04:27 +0530

    ip issue fix

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const PORT = 3000;
 app.get('/', async (req, res) => {
     // Get the user's IP address (behind proxy or not)
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    ip = ip.split(',')[0].trim(); // 👈 take only the first IP
 
     try {
         const geoRes = await fetch(`https://ipinfo.io/${ip}/json?token=${process.env.IP_INFO_TOKEN}`);
